4bf9cfdcc0ce278983ff7d36d70874e9b4d28dda
Uian Sol <sol.uian@gmail.com>
2021-04-27 11:56:38 -0300

    Time Conversion - Golang

---
+++ Hackerrank/Time Conversion.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func timeConversion(st string) string {
+	h, _ := strconv.Atoi(st[0:2])
+	m, _ := strconv.Atoi(st[3:5])
+	s, _ := strconv.Atoi(st[6:8])
+	p := st[8:10]
+
+	h = h % 12
+	if p == "PM" {
+		h += 12
+	}
+
+	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	s := readLine(reader)
+
+	result := timeConversion(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
