1b4b6836e125b2bb7aeffacb77d763d0dfce01ec
Uian Sol <sol.uian@gmail.com>
2021-04-27 11:41:11 -0300

    Mini-Max Sum - Golang

---
+++ Hackerrank/Mini-Max Sum.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr []int) {
+	sort.Ints(arr)
+	sum := sumIntSlice(arr)
+
+	fmt.Printf("%d ", sum-arr[len(arr)-1])
+	fmt.Printf("%d\n", sum-arr[0])
+}
+
+func sumIntSlice(arr []int) int {
+	sum := 0
+
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	}
+
+	return sum
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
