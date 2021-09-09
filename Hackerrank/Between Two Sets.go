7562cdb043ee8bb16d756f07646effb0267f3019
Uian Sol <sol.uian@gmail.com>
2021-09-09 17:05:47 -0300

    Between Two Sets - Golang

---
+++ Hackerrank/Between Two Sets.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getTotalX(a []int32, b []int32) int32 {
+	var sum, i int32
+	var ok bool
+
+	for i = 1; i <= 100; i++ {
+		ok = true
+
+		for _, n := range a {
+			if i%n != 0 {
+				ok = false
+				break
+			}
+		}
+		for _, n := range b {
+			if n%i != 0 {
+				ok = false
+				break
+			}
+		}
+
+		if ok {
+			sum += 1
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	mTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	brrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var brr []int32
+
+	for i := 0; i < int(m); i++ {
+		brrItemTemp, err := strconv.ParseInt(brrTemp[i], 10, 64)
+		checkError(err)
+		brrItem := int32(brrItemTemp)
+		brr = append(brr, brrItem)
+	}
+
+	total := getTotalX(arr, brr)
+
+	fmt.Fprintf(writer, "%d\n", total)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
