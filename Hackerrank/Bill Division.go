993f0df484b7af82fd37b191b3c3543d473d2eff
Uian Sol <usoldeveloper@gmail.com>
2022-03-04 17:31:31 -0300

    Bill Division - Go

---
+++ Hackerrank/Bill Division.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func bonAppetit(bill []int32, k int32, b int32) {
+	var total int32 = 0
+	for _, n := range bill {
+		total += n
+	}
+
+	anasBill := (total - bill[k]) / 2
+
+	if anasBill == b {
+		fmt.Println("Bon Appetit")
+	} else {
+		fmt.Println(b - anasBill)
+	}
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	billTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var bill []int32
+
+	for i := 0; i < int(n); i++ {
+		billItemTemp, err := strconv.ParseInt(billTemp[i], 10, 64)
+		checkError(err)
+		billItem := int32(billItemTemp)
+		bill = append(bill, billItem)
+	}
+
+	bTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	b := int32(bTemp)
+
+	bonAppetit(bill, k, b)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
