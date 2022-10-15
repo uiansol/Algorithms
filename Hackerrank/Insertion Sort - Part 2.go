7cb4bd33da6a0abe9a7aa38df8a2ae502d0a69eb
Uian Sol <usoldeveloper@gmail.com>
2022-10-15 14:55:17 -0300

    Hackerrank - Insertion Sort - Part 2 - Golang

---
+++ Hackerrank/Insertion Sort - Part 2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func insertionSort2(n int32, arr []int32) {
+	for i := int32(1); i < n; i++ {
+		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+		}
+		temp := fmt.Sprintf("%v", arr)
+		fmt.Println(strings.Join(strings.Split(temp[1:len(temp)-1], " "), " "))
+	}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	insertionSort2(n, arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
