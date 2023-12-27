44ebae39ec950f6e5bec97a7247115422eb2cf8c
Uian Sol <usoldeveloper@gmail.com>
2023-12-27 16:13:55 -0300

    Hackerrank - Pangrams - Golang

---
+++ Hackerrank/Pangrams.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func pangrams(s string) string {
+	str := strings.ToLower(s)
+	mapStr := make(map[rune]int)
+
+	for _, v := range str {
+		if v >= 'a' && v <= 'z' {
+			mapStr[v]++
+		}
+	}
+
+	if len(mapStr) == 26 {
+		return "pangram"
+	}
+
+	return "not pangram"
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	s := readLine(reader)
+
+	result := pangrams(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
