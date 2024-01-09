f1e3100b04dc20a46f1366a42162abf3c67929fc
Uian Sol <usoldeveloper@gmail.com>
2024-01-09 09:40:48 -0300

    Leetcode - Maximum Subarray - Golang 2

---
+++ Leetcode/Maximum Subarray/maximum_subarray_2.go
@@ -0,0 +1,16 @@
+// Accepted  125 ms  8.4 MB
+
+func maxSubArray(nums []int) int {
+	l := len(nums)
+	maxSubarray := nums[0]
+
+	for i := 0; i < l; i++ {
+		currentSubarray := 0
+		for j := i; j < l; j++ {
+			currentSubarray += nums[j]
+			maxSubarray = max(maxSubarray, currentSubarray)
+		}
+	}
+
+	return maxSubarray
+}
