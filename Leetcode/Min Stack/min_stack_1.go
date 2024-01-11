872cc462444e8aeb1d873a714e1b3df09bc9cb10
Uian Sol <usoldeveloper@gmail.com>
2024-01-11 09:55:48 -0300

    Leetcode - Min Stack - Golang 1

---
+++ Leetcode/Min Stack/min_stack_1.go
@@ -0,0 +1,32 @@
+// Accepted  16 ms  6.91 MB
+
+type MinStack struct {
+	data []int // elements stack
+	mins []int // minimums stack
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (s *MinStack) Push(val int) {
+	if len(s.mins) == 0 || val <= s.GetMin() {
+		s.mins = append(s.mins, val)
+	}
+	s.data = append(s.data, val)
+}
+
+func (s *MinStack) Pop() {
+	if s.Top() == s.GetMin() {
+		s.mins = s.mins[:len(s.mins)-1]
+	}
+	s.data = s.data[:len(s.data)-1]
+}
+
+func (s *MinStack) Top() int {
+	return s.data[len(s.data)-1]
+}
+
+func (s *MinStack) GetMin() int {
+	return s.mins[len(s.mins)-1]
+}
