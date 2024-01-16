3b9f94cfe6f524e54855fe55bce09871dccb2c36
Uian Sol <usoldeveloper@gmail.com>
2024-01-16 11:03:57 -0300

    Leetcode - Missing Number - C++ 2

---
+++ Leetcode/Easy/Missing Number/Missing Number_2.cpp
@@ -0,0 +1,14 @@
+// Accepted  8 ms  18.24 MB
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int missing = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            missing ^= i ^ nums[i];
+        }
+
+        return missing;
+    }
+};
