5d2cfea11de70f94a8d6f446d2c8d6daeb64f43a
Uian Sol <usoldeveloper@gmail.com>
2024-01-23 23:43:23 -0300

    Leetcode - Remove Element - Golang 2

---
+++ Leetcode/Algorithms/Easy/Remove Element/Remove Element_2.go
@@ -0,0 +1,15 @@
+func removeElement(nums []int, val int) int {
+	i, j := 0, len(nums)-1
+
+	for i <= j {
+		if nums[i] == val {
+			nums[i] = nums[j]
+			j--
+		} else {
+			i++
+		}
+
+	}
+
+	return i
+}
