e66e3fab0cdc9aa833b8bec9eea62ea6a117381e
Uian Sol <usoldeveloper@gmail.com>
2024-02-14 10:22:51 -0300

    Leetcode - Sales Person - SQL

---
+++ Leetcode/Database/Easy/Sales Person.sql
@@ -0,0 +1,16 @@
+SELECT
+    sp.name
+FROM
+    SalesPerson sp
+WHERE
+    sp.sales_id NOT IN (
+        SELECT
+            o.sales_id
+        FROM
+            Orders o
+            LEFT JOIN
+            Company c ON o.com_id = c.com_id
+        WHERE
+            c.name = 'RED'
+    )
+;
