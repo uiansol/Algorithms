9e073766ebdb87ec935d9806ad83aeba2dd2e3b8
Uian Sol <usoldeveloper@gmail.com>
2024-06-27 17:39:18 -0300

    Leetcode - Squares of a Sorted Array - Golang 2

---
+++ Leetcode/Algorithms/Easy/Squares of a Sorted Array/Squares of a Sorted Array_2.go
@@ -0,0 +1,28 @@
+func sortedSquares(nums []int) []int {
+	var temp []int
+	idx, nIdx := -1, 0
+
+	for _, v := range nums {
+		squared := v * v
+		if v < 0 {
+			temp = append(temp, squared)
+			idx += 1
+		} else {
+			for idx >= 0 && squared > temp[idx] && idx >= 0 {
+				nums[nIdx] = temp[idx]
+				nIdx++
+				idx--
+			}
+			nums[nIdx] = squared
+			nIdx++
+		}
+	}
+
+	for idx >= 0 {
+		nums[nIdx] = temp[idx]
+		nIdx++
+		idx--
+	}
+
+	return nums
+}
